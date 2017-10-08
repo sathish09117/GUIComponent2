87f6d7db9be1e80a074c34b998612d0198768dc7
sathish kumar <sathish kumar>
2017-10-08 16:21:17 +0530

    Initial commit

---
+++ app/src/main/java/demo/vvsn/info/guicomponent2/Splashscreen.java
@@ -0,0 +1,13 @@
+package demo.vvsn.info.guicomponent2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+    }
+}
